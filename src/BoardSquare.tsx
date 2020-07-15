586771b604d5ed1167906c28ea1d801a0463e684
ryota-murakami <dojce1048@gmail.com>
2020-07-15 18:01:31 +0900

    to Add a Drag Preview Image

--- src/BoardSquare.tsx
+++ src/BoardSquare.tsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import Square from './Square'
+import Overlay from './Overlay'
 import { canMoveKnight, moveKnight } from './Game'
 import { ItemTypes } from './Constants'
 import { useDrop } from 'react-dnd'
@@ -11,12 +12,13 @@ interface Props {
 
 const BoardSquare: React.FC<Props> = ({ x, y, children }) => {
   const black = (x + y) % 2 === 1
-  const [{ isOver }, drop] = useDrop({
+  const [{ isOver, canDrop }, drop] = useDrop({
     accept: ItemTypes.KNIGHT,
-    canDrop: () => canMoveKnight(x, y),
     drop: () => moveKnight(x, y),
+    canDrop: () => canMoveKnight(x, y),
     collect: (monitor) => ({
       isOver: !!monitor.isOver(),
+      canDrop: !!monitor.canDrop(),
     }),
   })
 
@@ -30,20 +32,9 @@ const BoardSquare: React.FC<Props> = ({ x, y, children }) => {
       }}
     >
       <Square black={black}>{children}</Square>
-      {isOver && (
-        <div
-          style={{
-            position: 'absolute',
-            top: 0,
-            left: 0,
-            height: '100%',
-            width: '100%',
-            zIndex: 1,
-            opacity: 0.5,
-            backgroundColor: 'yellow',
-          }}
-        />
-      )}
+      {isOver && !canDrop && <Overlay color="red" />}
+      {!isOver && canDrop && <Overlay color="yellow" />}
+      {isOver && canDrop && <Overlay color="green" />}
     </div>
   )
 }
